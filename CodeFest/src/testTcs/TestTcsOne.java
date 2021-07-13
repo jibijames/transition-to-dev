2a28166f819003d4fe28dd2a6d9f2309e92d225e
jjames <jjames@in.rm.com>
2021-07-13 14:09:52 +0530

    tcs test

--- CodeFest/src/testTcs/TestTcsOne.java
+++ CodeFest/src/testTcs/TestTcsOne.java
@@ -3,16 +3,27 @@ package testTcs;
 public class TestTcsOne {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		int N=4; // No of Students 
+		int K=3; // No: of classes =2
+		int[] Capacity = {1,1,2}; // capacity of each class = {1,1}
+		String[] Preference={"1","1,2","3"}; //preference of each student. utmost two consecutive pref {"1","1,2"}	
+			
+		System.out.println(solution(N,K,Capacity,Preference));
 
 	}
 	
 	public static String solution(int N,int K,int[] Capacity, String[] Preference) {
-		//No of Students = 2
-		//No: of classes =2
-		// capacity of each class = {1,1}
-		//preference of each student. utmost two consecutive pref {"1","1,2"}
-		return "";
+			
+		int totalCapacity=0;
+		for(int itr=0;itr<K;itr++) {
+			totalCapacity=totalCapacity+Capacity[itr];
+		}
+		if(totalCapacity<N) {
+			return "No";
+		}else {
+			return	"Yes";
+		}
+		
 	}
 
 }
