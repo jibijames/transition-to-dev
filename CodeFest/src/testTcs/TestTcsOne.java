991629584fb95c354e0389b98dc2bee4f5868819
jjames <jjames@in.rm.com>
2021-07-13 21:40:59 +0530

    tcs Class distribution problem solution

--- CodeFest/src/testTcs/TestTcsOne.java
+++ CodeFest/src/testTcs/TestTcsOne.java
@@ -1,13 +1,16 @@
 package testTcs;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class TestTcsOne {
 
 	public static void main(String[] args) {
 		int N=4; // No of Students 
 		int K=3; // No: of classes =2
-		int[] Capacity = {1,1,2}; // capacity of each class = {1,1}
-		String[] Preference={"1","1,2","3"}; //preference of each student. utmost two consecutive pref {"1","1,2"}	
-			
+		int[] Capacity = {1,2,3}; // capacity of each class = {1,1}
+		String[] Preference={"1","1,2","2","2,3"}; //preference of each student. utmost two consecutive pref {"1","1,2"}	
+		//solution for Class distribution problem 	
 		System.out.println(solution(N,K,Capacity,Preference));
 
 	}
@@ -18,10 +21,44 @@ public class TestTcsOne {
 		for(int itr=0;itr<K;itr++) {
 			totalCapacity=totalCapacity+Capacity[itr];
 		}
+		System.out.println("totalCapacity:"+totalCapacity);
 		if(totalCapacity<N) {
 			return "No";
 		}else {
-			return	"Yes";
+			Map<String,Integer> classCapacity = new HashMap<String,Integer>();
+			for(int itr=0;itr<K;itr++) {
+				classCapacity.put("capacity_"+(itr+1), Capacity[itr]);
+			}
+			System.out.println(classCapacity.toString());
+			for(int itr=0;itr<N;itr++) {
+				String studentPreference=Preference[itr];
+				if(studentPreference.contains(",")) {
+					String[] tempPrefs=studentPreference.split(",");
+					String firstPref=tempPrefs[0];
+					String secondPref=tempPrefs[1];
+					int firstCapacity=classCapacity.get("capacity_"+firstPref);
+					firstCapacity--;
+					if(firstCapacity<0) {
+						int secondCapacity=classCapacity.get("capacity_"+secondPref);
+						secondCapacity--;
+						classCapacity.put("capacity_"+secondPref, secondCapacity);
+					}else {
+						classCapacity.put("capacity_"+firstPref, firstCapacity);
+					}					
+				}else {
+					int firstCapacity=classCapacity.get("capacity_"+studentPreference);
+					firstCapacity--;
+					classCapacity.put("capacity_"+studentPreference, firstCapacity);
+				}
+				
+			}
+			System.out.println(classCapacity.toString());
+			for (var entry : classCapacity.entrySet()) {
+			    if(entry.getValue()<0) {
+			    	return "No";
+			    }
+			}
+			return "Yes";
 		}
 		
 	}
