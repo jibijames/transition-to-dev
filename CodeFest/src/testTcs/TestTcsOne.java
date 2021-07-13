9607e7ce84ac341af6e8767a2e1e8f447df2e9c3
jibijames <jibijames07@gmail.com>
2021-07-13 12:45:29 +0530

    Tcs test

---
+++ CodeFest/src/testTcs/TestTcsOne.java
@@ -0,0 +1,18 @@
+package testTcs;
+
+public class TestTcsOne {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public static String solution(int N,int K,int[] Capacity, String[] Preference) {
+		//No of Students = 2
+		//No: of classes =2
+		// capacity of each class = {1,1}
+		//preference of each student. utmost two consecutive pref {"1","1,2"}
+		return "";
+	}
+
+}
